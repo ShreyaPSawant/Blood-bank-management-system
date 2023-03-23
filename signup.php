91a676d0f54e94b6bfdd686b9d73c764cd159237
ShreyaPSawant <89719271+ShreyaPSawant@users.noreply.github.com>
2023-03-23 20:30:34 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,50 @@
+
+<form action="insert2.php" method="post">
+    <div class="form-group">
+    	<label>Name</label>
+    	<input class="form-control form-control-lg" type="text" name="name" placeholder="Enter your name">
+    </div>
+    <div class="form-group">
+    	<label>username</label>
+    	<input class="form-control form-control-lg" type="text" name="u_name" placeholder="Enter your username">
+    </div>
+    <div class="form-group">
+    	<label>Email</label>
+    	<input class="form-control form-control-lg" type="email" name="u_email" placeholder="Enter your email">
+    </div>
+    <div class="form-group">
+    	<label>Phone number</label>
+    	<input class="form-control form-control-lg" type="text" name="u_phone" placeholder="Enter your phone number">
+    </div>
+    <div class="form-group">
+    	<label>Address</label>
+    	<input class="form-control form-control-lg" type="text" name="u_add" placeholder="Enter your address">
+    </div>
+    <div class="form-group">
+    	<label>Blood group</label>
+    	<input class="form-control form-control-lg" type="text" name="u_bloodgrp" placeholder="Enter your blood group">
+    </div>
+    <div class="form-group">
+    	<label>DOB</label>
+    	<input class="form-control form-control-lg" type="datetime-local" name="u_dob" placeholder="Enter your date of birth">
+    </div>
+    <div class="form-group">
+    	<label>Medical issues </label>
+    	<input class="form-control form-control-lg" type="text" name="u_mediss" placeholder="if no issues write none">
+    </div>
+    <div class="form-group">
+    	<label>Password</label>
+    	<input class="form-control form-control-lg" type="password" name="u_password1" placeholder="Enter password">
+    </div>
+    <div class="form-group">
+    	<label>Re-enter Password</label>
+    	<input class="form-control form-control-lg" type="password" name="u_password2" placeholder="Re-Enter password">
+    </div>
+    <div class="text-center mt-3" id="btn1">
+    	 <button type="submit" class="btn btn-lg btn-primary">Sign up</button> <br><br>
+    </div>
+    <label align="center">already a member?</label>
+    <div class="text-center mt-3" >
+    	<a href="login.php">Click to Login</a><br><br>
+    </div>
+</form>
